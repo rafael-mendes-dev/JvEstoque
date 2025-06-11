c629c34a5125cadfd68bf4cce1635f9d54854733
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-11 11:16:22 -0300

    Criado request para multiplas paginas

---
+++ JvEstoque.Core/Requests/PagedRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace JvEstoque.Core.Requests;
+
+public class PagedRequest
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = Configuration.DefaultPageSize;
+}

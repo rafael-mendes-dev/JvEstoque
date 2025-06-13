432c0207d2e66411c4452d6a353f25eac903be4f
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-13 09:45:10 -0300

    Criado requests para o estoque

---
+++ JvEstoque.Core/Requests/Estoques/CreateEstoqueRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JvEstoque.Core.Requests.Estoques;
+
+public class CreateEstoqueRequest
+{
+    [Required(ErrorMessage = "O ID do produto é obrigatório.")]
+    public int VariacaoProdutoId { get; set; }
+    
+    [Required(ErrorMessage = "A quantidade é obrigatória.")]
+    [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero.")]
+    public int Quantidade { get; set; }
+}

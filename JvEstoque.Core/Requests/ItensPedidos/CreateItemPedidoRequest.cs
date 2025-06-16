3207188dd3a99cbdfb5e4d8c9433b83c4b0ca396
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-16 10:15:24 -0300

    Corrigido requests para itens dos pedidos

--- JvEstoque.Core/Requests/ItensPedidos/CreateItemPedidoRequest.cs
+++ JvEstoque.Core/Requests/ItensPedidos/CreateItemPedidoRequest.cs
@@ -4,10 +4,15 @@ namespace JvEstoque.Core.Requests.ItensPedidos;
 
 public class CreateItemPedidoRequest
 {
+    public int PedidoId { get; set; }
     public int VariacaoProdutoId { get; set; }
     
     [Required(ErrorMessage = "A quantidade é obrigatória.")]
     [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero.")]
     public int Quantidade { get; set; }
+    
+    [Required(ErrorMessage = "O valor unitário é obrigatório.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O valor unitário deve ser maior que zero.")]
+    public decimal ValorUnitario { get; set; }
     
 }
--- JvEstoque.Core/Requests/ItensPedidos/UpdateItemPedidoRequest.cs
+++ JvEstoque.Core/Requests/ItensPedidos/UpdateItemPedidoRequest.cs
@@ -11,4 +11,8 @@ public class UpdateItemPedidoRequest
     [Required(ErrorMessage = "A quantidade é obrigatória.")]
     [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero.")]
     public int Quantidade { get; set; }
+    
+    [Required(ErrorMessage = "O valor unitário é obrigatório.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O valor unitário deve ser maior que zero.")]
+    public decimal ValorUnitario { get; set; }
 }

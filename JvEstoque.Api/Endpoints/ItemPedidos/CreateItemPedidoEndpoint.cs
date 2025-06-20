e72a993d65079930e00b4032758daf236745520f
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-20 12:03:14 -0300

    Criado endpoints para itempedidos

---
+++ JvEstoque.Api/Endpoints/ItemPedidos/CreateItemPedidoEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using JvEstoque.Api.Common.Api;
+using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.ItensPedidos;
+using JvEstoque.Core.Responses;
+
+namespace JvEstoque.Api.Endpoints.ItemPedidos;
+
+public class CreateItemPedidoEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .WithName("ItemPedidos : Create")
+            .WithSummary("Cria um novo item de pedido")
+            .WithDescription("Cria um novo item de pedido")
+            .WithOrder(1)
+            .Produces<Response<ItemPedido?>>();
+    
+    private static async Task<IResult> HandleAsync(
+        IItemPedidoHandler handler,
+        CreateItemPedidoRequest request)
+    {
+        var result = await handler.CreateAsync(request);
+        return result.IsSucess ? TypedResults.Created($"/{result.Data?.Id}", result) : TypedResults.BadRequest(result.Data);
+    }
+}
---
+++ JvEstoque.Api/Endpoints/ItemPedidos/GetItemPedidoByIdEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using JvEstoque.Api.Common.Api;
+using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.ItensPedidos;
+using JvEstoque.Core.Responses;
+
+namespace JvEstoque.Api.Endpoints.ItemPedidos;
+
+public class GetItemPedidoByIdEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{id:int}", HandleAsync)
+            .WithName("ItemPedidos : GetById")
+            .WithSummary("Obtém um item de pedido pelo ID")
+            .WithDescription("Obtém um item de pedido pelo ID")
+            .WithOrder(2)
+            .Produces<Response<ItemPedido?>>();
+    
+    private static async Task<IResult> HandleAsync(
+        IItemPedidoHandler handler,
+        int id)
+    {
+        var request = new GetItemPedidoByIdRequest()
+        {
+            Id = id
+        };
+        
+        var result = await handler.GetByIdAsync(request);
+        return result.IsSucess ? TypedResults.Ok(result) : TypedResults.NotFound(result.Data);
+    }
+}

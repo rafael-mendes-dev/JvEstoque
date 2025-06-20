0224396c72fbb207c0f747ed0677fab8e5686c65
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-20 11:47:08 -0300

    Criado endpoints para estoque

---
+++ JvEstoque.Api/Endpoints/Estoques/CreateEstoqueEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using JvEstoque.Api.Common.Api;
+using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.Estoques;
+using JvEstoque.Core.Responses;
+
+namespace JvEstoque.Api.Endpoints.Estoques;
+
+public class CreateEstoqueEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .WithName("Estoques : Create")
+            .WithSummary("Cria um estoque")
+            .WithDescription("Cria um novo estoque com as informações fornecidas")
+            .WithOrder(1)
+            .Produces<Response<Estoque?>>();
+    
+    public static async Task<IResult> HandleAsync(
+        IEstoqueHandler handler,
+        CreateEstoqueRequest request)
+    {
+        var result = await handler.CreateAsync(request);
+        return result.IsSucess ? TypedResults.Created("/{result.Data?.Id}", result) : TypedResults.BadRequest(result.Data);
+    }
+}
---
+++ JvEstoque.Api/Endpoints/Estoques/UpdateEstoqueEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using JvEstoque.Api.Common.Api;
+using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.Estoques;
+using JvEstoque.Core.Responses;
+
+namespace JvEstoque.Api.Endpoints.Estoques;
+
+public class UpdateEstoqueEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPut("/{id:int}", HandleAsync)
+            .WithName("Estoques : Update")
+            .WithSummary("Atualiza um estoque")
+            .WithDescription("Atualiza as informações de um estoque existente pelo ID")
+            .WithOrder(2)
+            .Produces<Response<Estoque?>>();
+    
+    public static async Task<IResult> HandleAsync(
+        IEstoqueHandler handler,
+        int id,
+        UpdateEstoqueRequest request)
+    {
+        request.Id = id;
+        var result = await handler.UpdateAsync(request);
+        return result.IsSucess ? TypedResults.Ok(result) : TypedResults.BadRequest(result.Data);
+    }
+}

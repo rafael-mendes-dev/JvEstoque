a4a14b5d58721c7c845f6589949e6f65038eb93e
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-23 17:44:33 -0300

    Corrigido mapeamento para variacao de produtos

--- JvEstoque.Api/Data/Mappings/VariacaoProdutoMapping.cs
+++ JvEstoque.Api/Data/Mappings/VariacaoProdutoMapping.cs
@@ -15,7 +15,6 @@ public class VariacaoProdutoMapping : IEntityTypeConfiguration<VariacaoProduto>
         builder.Property(vp => vp.Cor).IsRequired().HasMaxLength(30).HasColumnType("NVARCHAR(30)");
         builder.Property(vp => vp.Tecido).IsRequired(false).HasMaxLength(50).HasColumnType("NVARCHAR(50)");
         builder.Property(vp => vp.Genero).IsRequired().HasColumnType("SMALLINT");
-        builder.Property(vp => vp.FotoUrl).IsRequired(false).HasMaxLength(200).HasColumnType("NVARCHAR(200)");
         
         builder.HasOne(vp => vp.Produto)
             .WithMany(p => p.Variacoes)

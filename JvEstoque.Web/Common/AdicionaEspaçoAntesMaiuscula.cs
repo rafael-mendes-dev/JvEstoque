8a10956f2f57998c5626e5f5b1a84ea0142b2e95
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-07-01 13:57:03 -0300

    Criado função para adição de espaço antes de cada letra maiscula

---
+++ JvEstoque.Web/Common/AdicionaEspaçoAntesMaiuscula.cs
@@ -0,0 +1,9 @@
+﻿namespace JvEstoque.Web.Common;
+
+public static class AdicionaEspaçoAntesMaiuscula
+{
+    public static string AdicionarEspacoAntesMaiuscula(this string texto)
+    {
+        return System.Text.RegularExpressions.Regex.Replace(texto, "(?<!^)([A-Z])", " $1");
+    }
+}

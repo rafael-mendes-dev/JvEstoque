a14eba495d5654bcdcf337c507dc5001b94bea54
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-07-06 16:02:05 -0300

    Corrigido navmenu, responsividade da appbar e configurações para correção de bugs ao utilizar css isolado

---
+++ JvEstoque.Web/Components/Pedidos/CreatePedidoComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.Escolas;
+using JvEstoque.Core.Requests.Pedidos;
+using JvEstoque.Core.Requests.VariacoesProdutos;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace JvEstoque.Web.Components.Pedidos;
+
+public partial class CreatePedidoComponent : ComponentBase
+{
+    
+}
---
+++ JvEstoque.Web/Components/Pedidos/EditPedidoComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using JvEstoque.Core.Handlers;
+using JvEstoque.Core.Models;
+using JvEstoque.Core.Requests.Escolas;
+using JvEstoque.Core.Requests.ItensPedidos;
+using JvEstoque.Core.Requests.Pedidos;
+using JvEstoque.Core.Requests.VariacoesProdutos;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace JvEstoque.Web.Components.Pedidos;
+
+public partial class EditPedidoComponent : ComponentBase
+{
+    
+}

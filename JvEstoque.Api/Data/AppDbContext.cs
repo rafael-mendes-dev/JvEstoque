f3c52042254f1b33295a9cd367a1d90af41601b2
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-07-11 19:27:26 -0300

    Configurado DbContext para utilizacao das views

--- JvEstoque.Api/Data/AppDbContext.cs
+++ JvEstoque.Api/Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using JvEstoque.Api.Models;
 using JvEstoque.Core.Models;
+using JvEstoque.Core.Models.Reports;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -16,15 +17,24 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     IdentityRoleClaim<int>, 
     IdentityUserToken<int>> (options)
 {
-    public DbSet<Escola> Escolas { get; set; }
-    public DbSet<Estoque> Estoques { get; set; }
-    public DbSet<ItemPedido> ItensPedidos { get; set; }
-    public DbSet<Pedido> Pedidos { get; set; }
-    public DbSet<Produto> Produtos { get; set; }
-    public DbSet<VariacaoProduto> VariacoesProdutos { get; set; }
+    public DbSet<Escola> Escolas { get; set; } = null!;
+    public DbSet<Estoque> Estoques { get; set; } = null!;
+    public DbSet<ItemPedido> ItensPedidos { get; set; } = null!;
+    public DbSet<Pedido> Pedidos { get; set; } = null!;
+    public DbSet<Produto> Produtos { get; set; } = null!;
+    public DbSet<VariacaoProduto> VariacoesProdutos { get; set; } = null!;
+    public DbSet<FaturamentoReports> Faturamento { get; set; } = null!;
+    public DbSet<ItensEmBaixoEstoqueReport> ItensEmBaixoEstoque { get; set; } = null!;
+    public DbSet<PedidosConcluidosReport> PedidosConcluidos { get; set; } = null!;
+    public DbSet<QuantidadeDePedidosPorStatusReport> QuantidadeDePedidosPorStatus { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<FaturamentoReports>().HasNoKey().ToView("vwFaturamentoReport");
+        modelBuilder.Entity<ItensEmBaixoEstoqueReport>().HasNoKey().ToView("vwItensEmBaixoEstoqueReport");
+        modelBuilder.Entity<PedidosConcluidosReport>().HasNoKey().ToView("vwPedidosConcluidosReport");
+        modelBuilder.Entity<QuantidadeDePedidosPorStatusReport>().HasNoKey().ToView("vwQuantidadeDePedidosPorStatusReport");
     }
 }
